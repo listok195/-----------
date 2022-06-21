bee4535c4d9861768885e501e8e6ec9d3b14d7dd
listok195 <listok195@gmail.com>
2022-06-22 01:56:03 +0300

    п

--- 5JS/script.js
+++ 5JS/script.js
@@ -30,14 +30,16 @@ function rez1(a) {
     if (z1v1.value !== '') {
         a = Number(a)
         if (Number.isInteger(a)) {
-            let b = 1
-            let c = a
-            let d = 0
-            task1(a, b, c, d)
-            rezult(r1, b)
-        }
-        else
-            rezult(r1, 'Неверно')
+            if (a > 0) {
+                let b = 1
+                let c = a
+                let d = 0
+                task1(a, b, c, d)
+                rezult(r1, b)
+            } else {
+                r5.innerHTML = 'Неверно'
+            }
+        } else { r5.innerHTML = 'Неверный тип данных' }
     }
     else {
         rezult(r1, 'Нет зачений')
@@ -123,10 +125,14 @@ function rez3(a) {
         a = Number(a)
         let b = ''
         if (Number.isInteger(a)) {
-            a = String(a)
-            let i = a.length - 1
-            task3(a, b, i)
-        }
+            if (a > 0) {
+                a = String(a)
+                let i = a.length - 1
+                task3(a, b, i)
+            } else {
+                r5.innerHTML = 'Неверно'
+            }
+        } else { r5.innerHTML = 'Неверный тип данных' }
     }
     else {
 
@@ -158,15 +164,65 @@ function rez4(a) {
     if (z4v1.value !== '') {
         a = Number(a)
         if (Number.isInteger(a)) {
-            a = String(a)
-            let b = 0
-            let i = 0
-            task4(a, b, i)
-        }
+            if (a > 0) {
+                a = String(a)
+                let b = 0
+                let i = 0
+                task4(a, b, i)
+            } else {
+                r5.innerHTML = 'Неверно'
+            }
+        } else { r5.innerHTML = 'Неверный тип данных' }
     }
     else {
 
         r4.innerHTML = 'Нет зачений'
+    }
+
+}
+// 5
+function task5(a, b, i) {
+    if (i == 0) {
+        r5.innerHTML += "("
+        b++
+        if (a == b)
+            i = 1
+    }
+    else {
+        r5.innerHTML += ")"
+        b--
+        if (b == 0) { i = 2 }
+    }
+    if (i == 1 || i == 0)
+        task5(a, b, i)
+}
+z5btn.onclick = function (e) {
+    rez5(z5v1.value)
+}
+for (item of document.querySelectorAll('#z5 input')) {
+    item.addEventListener('keypress', function (e) {
+        if (e.key == 'Enter') {
+            rez5(z5v1.value)
+        }
+    })
+}
+function rez5(a) {
+    if (z5v1.value !== '') {
+        a = Number(a)
+        if (Number.isInteger(a)) {
+            if (a > 0) {
+                r5.innerHTML = ''
+                let i = 0
+                let b = 0
+                task5(a, b, i)
+            } else {
+                r5.innerHTML = 'Неверно'
+            }
+        } else { r5.innerHTML = 'Неверный тип данных' }
+    }
+    else {
+
+        r5.innerHTML = 'Нет зачений'
     }
 
 }
