6bd7ed186759efe206b433139db54870ff275ecf
it.step.sochi@outlook.com <it.step.sochi@outlook.com>
2022-06-24 21:29:45 +0300

    ghh

--- hom/JSMOD2/1JS/script.js
+++ hom/JSMOD2/1JS/script.js
@@ -1,46 +1,230 @@
-// function rezult(obj, text) {
-//     let paddingR = (90 - text.length * 6 < 5) ? 5 : 90 - text.length * 6
-//     // let paddingL = ()
-//     obj.style.paddingRight = paddingR + 'px'
-//     obj.innerHTML = text
-// }
-
-// 1
-
-function createObject(a, b) {
-    return { a, b }
+// Определение объекта
+rectangle = {
+    ax: 0,
+    ay: 0,
+    cx: 0,
+    cy: 0
 }
-
-z1btn.onclick = function (e) {
-    rez1(z1v1.value)
+function app() { rez1(); rez2(); rez3(); rez4(); rez5(); }
+function apprr() {
+    if (z111v1.value !== '' && z111v2.value !== '' && z111v3.value !== '' && z111v4.value) {
+        if (Number(z111v1.value) < Number(z111v3.value) && Number(z111v2.value) < Number(z111v4.value)) {
+            rectangle = {
+                ax: Number(z111v1.value),
+                ay: Number(z111v2.value),
+                cx: Number(z111v3.value),
+                cy: Number(z111v4.value)
+            }
+            app()
+        } else {
+            r1.innerHTML = 'Неверное значение'
+        }
+    }
+    else {
+        r1.innerHTML = 'Нет зачений'
+    }
 }
-for (item of document.querySelectorAll('#z1 input')) {
+z111btn.onclick = function (e) {
+    apprr()
+}
+for (item of document.querySelectorAll('#z111 input')) {
     item.addEventListener('keypress', function (e) {
         if (e.key == 'Enter') {
+            if (z111v1.value !== '' && z111v2.value !== '' && z111v3.value !== '' && z111v4.value) {
+                if (Number(z111v1.value) < Number(z111v3.value) && Number(z111v2.value) < Number(z111v4.value)) {
+                    rectangle = {
+                        ax: Number(z111v1.value),
+                        ay: Number(z111v2.value),
+                        cx: Number(z111v3.value),
+                        cy: Number(z111v4.value)
+                    }
+                    app()
+                } else {
+                    r1.innerHTML = 'Неверное значение'
+                }
+            }
+            else {
+                r1.innerHTML = 'Нет зачений'
+            }
 
-            rez1(z1v1.value)
         }
     })
 }
-function rez1(a) {
-    if (z1v1.value !== '') {
-        a = Number(a)
-        if (Number.isInteger(a)) {
-            if (a > 0) {
-                let b = 1
-                let c = a
-                let d = 0
-                task1(a, b, c, d)
-                rezult(r1, b)
-            } else {
-                r5.innerHTML = 'Неверно'
-            }
-        } else { r5.innerHTML = 'Неверный тип данных' }
-    }
-    else {
-        rezult(r1, 'Нет зачений')
+function rez1() {
+    r1.innerHTML = `A(${rectangle.ax};${rectangle.ay})<br>C(${rectangle.cx};${rectangle.cy})`
+}
+function rez2() {
+    r2.innerHTML = rectangle.cx - rectangle.ax
+}
+function rez3() {
+    r3.innerHTML = rectangle.cy - rectangle.ay
+}
+function rez4() {
+    r4.innerHTML = (rectangle.cx - rectangle.ax) * (rectangle.cy - rectangle.ay)
+}
+function rez5() {
+    r5.innerHTML = (rectangle.cx + rectangle.ax + rectangle.cy + rectangle.ay) * 2
+}
+// 6
+z6btn.onclick = function (e) {
+    if (z6v1.value !== '') {
+        rez6(z6v1.value)
     }
 }
+for (item of document.querySelectorAll('#z111 input')) {
+    item.addEventListener('keypress', function (e) {
+        if (e.key == 'Enter') {
+            if (z6v1.value !== '') {
+                rez6(z6v1.value)
+            }
+
+        }
+    })
+}
+function rez6(a) {
+    if (z111v1.value !== '' && z111v2.value !== '' && z111v3.value !== '' && z111v4.value) {
+        if (Number.isInteger(Number(a))) {
+            if (a < 0) {
+                if (((Number(z111v3.value) - Number(z111v1.value)) + Number(a)) > 0) {
+
+                    z111v3.value = Number(z111v3.value) + Number(a)
+                    apprr()
+                }
+            }
+            else {
+                z111v3.value = Number(z111v3.value) + Number(a)
+                apprr()
+            }
+
+        }
+    }
+}
+// 7
+z7btn.onclick = function (e) {
+    if (z7v1.value !== '') {
+        rez7(z7v1.value)
+    }
+}
+for (item of document.querySelectorAll('#z111 input')) {
+    item.addEventListener('keypress', function (e) {
+        if (e.key == 'Enter') {
+            if (z7v1.value !== '') {
+                rez7(z7v1.value)
+            }
+
+        }
+    })
+}
+function rez7(a) {
+    if (z111v1.value !== '' && z111v2.value !== '' && z111v3.value !== '' && z111v4.value) {
+        if (Number.isInteger(Number(a))) {
+            if (a < 0) {
+                if (((Number(z111v4.value) - Number(z111v2.value)) + Number(a)) > 0) {
+
+                    z111v4.value = Number(z111v4.value) + Number(a)
+                    apprr()
+                }
+            }
+            else {
+                z111v4.value = Number(z111v4.value) + Number(a)
+                apprr()
+            }
+
+        }
+    }
+}
+// 8
+z8btn.onclick = function (e) {
+    if (z8v1.value !== '' && z8v2.value !== '') {
+        rez8(z8v1.value, z8v2.value)
+    }
+}
+for (item of document.querySelectorAll('#z111 input')) {
+    item.addEventListener('keypress', function (e) {
+        if (e.key == 'Enter') {
+            if (z8v1.value !== '') {
+                rez8(z8v1.value, z8v2.value)
+            }
+
+        }
+    })
+}
+function rez8(a, b) {
+
+    if (z111v1.value !== '' && z111v2.value !== '') {
+        if (Number.isInteger(Number(a))) {
+            if (a > 0) {
+                z111v3 = Number(z111v1) + a
+                apprr()
+            }
+
+
+        }
+        if (Number.isInteger(Number(b))) {
+            if (b > 0) {
+                z111v4.value = Number(z111v2) + b
+                apprr()
+            }
+
+
+        }
+    }
+}
+// 9
+z9btn.onclick = function (e) {
+    if (z9v1.value !== '') {
+        rez9(z9v1.value)
+    }
+}
+for (item of document.querySelectorAll('#z111 input')) {
+    item.addEventListener('keypress', function (e) {
+        if (e.key == 'Enter') {
+            if (z9v1.value !== '') {
+                rez9(z9v1.value)
+            }
+
+        }
+    })
+}
+function rez9(a) {
+
+    if (z111v1.value !== '' && z111v2.value !== '' && z111v3.value !== '' && z111v4.value) {
+        if (Number.isInteger(Number(a))) {
+            if (a > 0) {
+                z111v1.value = a
+                z111v3.value = a
+            }
+        }
+    }
+}
+// 10
+z10btn.onclick = function (e) {
+    if (z10v1.value !== '') {
+        rez10(z10v1.value)
+    }
+}
+for (item of document.querySelectorAll('#z111 input')) {
+    item.addEventListener('keypress', function (e) {
+        if (e.key == 'Enter') {
+            if (z10v1.value !== '') {
+                rez10(z10v1.value)
+            }
+
+        }
+    })
+}
+function rez10(a) {
+
+    if (z111v1.value !== '' && z111v2.value !== '' && z111v3.value !== '' && z111v4.value) {
+        if (Number.isInteger(Number(a))) {
+            if (a > 0) {
+                z111v2.value = a
+                z111v4.value = a
+            }
+        }
+    }
+}
+
 // if (this.value.length > 20) {
 //     z10v1Width += 5
 //     z10v1.style.width = z8v1Width + 'px'
