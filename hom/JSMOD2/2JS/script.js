1bee89f3dcbd8e1f398f9e61243dabd3c60d0c1d
it.step.sochi@outlook.com <it.step.sochi@outlook.com>
2022-06-29 21:32:05 +0300

    fix

--- hom/JSMOD2/2JS/script.js
+++ hom/JSMOD2/2JS/script.js
@@ -5,9 +5,10 @@ arrcrbtn.onclick = function (e) {
 }
 function arrcreate10() {
     for (let i = 0; i < 10; i++) {
-        arr10[i] = (Math.floor(Math.random() * 100))
+        arr10[i] = (Math.floor(Math.random() * 10))
     }
     rez1(arr10); rez2(arr10); rez3(arr10); rez4(arr10);
+    rez7(); rez8(); rez9()
 
 }
 arrcreate10()
@@ -42,13 +43,13 @@ function rez4(arr) {
 }
 // 5
 z5btn.onclick = function (e) {
-    rez5(z5v1.value, arr)
+    rez5(z5v1.value, arr10)
 
 }
 for (item of document.querySelectorAll('#z111 input')) {
     item.addEventListener('keypress', function (e) {
         if (e.key == 'Enter') {
-            rez5(z5v1.value, arr)
+            rez5(z5v1.value, arr10)
 
 
 
@@ -60,7 +61,7 @@ function rez5(a, arr) {
         if (Number.isInteger(Number(a))) {
             if (arr.length >= a && 0 <= a) {
                 arr.splice(a, 0, (Math.floor(Math.random() * 10)))
-                rez1(arr); rez2(arr); rez3(arr); rez4(arr); rez7(arr5); rez8(arr5); rez9(arr5)
+                rez1(arr); rez2(arr); rez3(arr); rez4(arr); rez7(); rez8(); rez9()
                 r5.innerHTML = 'Успех'
             }
         }
@@ -74,13 +75,13 @@ function rez5(a, arr) {
 }
 // 6
 z6btn.onclick = function (e) {
-    rez6(z6v1.value, arr)
+    rez6(z6v1.value, arr10)
 
 }
 for (item of document.querySelectorAll('#z111 input')) {
     item.addEventListener('keypress', function (e) {
         if (e.key == 'Enter') {
-            rez6(z6v1.value, arr)
+            rez6(z6v1.value, arr10)
 
 
 
@@ -92,7 +93,7 @@ function rez6(a, arr) {
         if (Number.isInteger(Number(a))) {
             if (arr.length >= a && 0 <= a) {
                 arr.splice(a, 1)
-                rez1(arr); rez2(arr); rez3(arr); rez4(arr); rez7(arr5); rez8(arr5); rez9(arr5)
+                rez1(arr); rez2(arr); rez3(arr); rez4(arr); rez7(); rez8(); rez9()
                 r6.innerHTML = 'Успех'
             }
         }
@@ -110,30 +111,113 @@ arrcrbtn2.onclick = function (e) {
 function arrcreate5() {
     let a = ''
     for (let i = 0; i < 5; i++) {
-        arr5[i] = (Math.floor(Math.random() * 100))
+        arr5[i] = (Math.floor(Math.random() * 10))
         a += `${arr5[i]} `
         r79.innerHTML = a
 
     }
-    rez7(); rez8(arr5); rez9(arr5)
+    rez7(); rez8(); rez9()
 
 }
 arrcreate5()
 // 7
 function rez7() {
-    let a
-    for (let i = 0; i < 10; i++) {
-        for (let j = 0; j < 5; j++) {
-            if (arr10[i] == arr5[j]) {
-                a += arr5[i]
+    const newArr = []
+    for (let i = 0; i < Math.max(arr5.length, arr10.length); i++) {
+
+        if (i < arr5.length) {
+            if (newArr.indexOf(arr5[i]) < 0) {
+                newArr.push(arr5[i])
             }
-            else {
-                a += arr5[j];
-                a += arr10[i]
+        }
+        if (i < arr10.length) {
+            if (newArr.indexOf(arr10[i]) < 0) {
+                newArr.push(arr10[i])
             }
-            r7.innerHTML = a
+        }
+    }
+    r7.innerHTML = newArr
+}
+function rez8() {
+    const newArr = []
+    let maxArr
+    let minArr
+    if (arr5.length > arr10.length) {
+        maxArr = arr5
+        minArr = arr10
+    } else {
+        maxArr = arr10
+        minArr = arr5
+    }
+    for (let i = 0; i < Math.max(arr5.length, arr10.length); i++) {
+        if (minArr.indexOf(maxArr[i]) > 0 && newArr.indexOf(maxArr[i]) < 0) {
+            newArr.push(maxArr[i])
+        }
+    }
+    r8.innerHTML = newArr
+}
+function rez9() {
+    const newArr = []
+    let maxArr
+    let minArr
+    if (arr5.length > arr10.length) {
+        maxArr = arr5
+        minArr = arr10
+    } else {
+        maxArr = arr10
+        minArr = arr5
+    }
+    for (let i = 0; i < Math.max(arr5.length, arr10.length); i++) {
+        if (minArr.indexOf(maxArr[i]) < 0) {
+            newArr.push(maxArr[i])
+        }
+    }
+    r9.innerHTML = newArr
+}
+let fruits = ['apple', 'orange', 'pineapple', 'cherry', 'pear']
+function rez10() {
+    ulEl = document.createElement('ul')
+
+    for (fruit of fruits) {
+        liEl = document.createElement('li')
+        liEl.innerHTML = fruit
+        ulEl.appendChild(liEl)
+    }
+
+    r10.appendChild(ulEl)
+}
+rez10()
+// 11
+z11btn.onclick = function (e) {
+    rez11(z11v1.value)
+
+}
+for (item of document.querySelectorAll('#z11 input')) {
+    item.addEventListener('keypress', function (e) {
+        if (e.key == 'Enter') {
+            rez11(z11v1.value)
+
+
+        }
+    })
+}
+function rez11(a) {
+    let b = a.toLowerCase()
+    r11.innerHTML = ''
+    if (z11v1.value !== '') {
+        for (let i = 0; i < fruits.length; i++) {
+            if (fruits[i] == b) {
+                r11.innerHTML = i
+                return 0
+            }
+        }
+        if (r11.innerHTML == '') {
+            r11.innerHTML = '-1'
         }
 
+    }
+    else {
+        r11.innerHTML = 'Нет зачений'
     }
 }
 // 5
